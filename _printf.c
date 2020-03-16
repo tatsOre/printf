fcd1e606a29e27bd9649caf04de00e8c017b39e4
AndrewB4y <andresbaymon@gmail.com>
2020-03-16 14:52:55 +0000

    Adding the file _printf.c

---
+++ _printf.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _printf - prints according to a given format. printf Implementation.
+ * @format: string holding characters and format of arguments to be printed.
+ *
+ * Return: number of charaters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list original, copy;
+	int n_printed = 0, error = 0;
+
+	if (format == NULL || *format == '\0')
+	{
+		/*Print error here*/
+		return (0);
+	}
+
+	va_start(original, format);
+	va_copy(copy, original);
+
+	error = check_input(&original, format);
+
+	if (error != 0)
+	{
+		handle_errors(error);
+		exit(error);
+	}
+
+	n_printed = print_output(copy, format);
+
+	return (n_printed);
+}
