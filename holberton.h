ae4d1f244bfe9cd1c471ab5d95f633de2c3a4391
AndrewB4y <andresbaymon@gmail.com>
2020-03-16 15:23:38 +0000

    Adding structure definition for special characters

--- holberton.h
+++ holberton.h
@@ -4,10 +4,23 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+/**
+ * struct form_t - Struct form_t
+ *
+ * @c: data type
+ * @f: The function associated
+ */
+typedef struct special_formats
+{
+	char *c;
+	void (*f)(va_list *);
+} s_format;
+
+int _printf(const char *, ...);
 int _putchar(char c);
 int check_input(const char *format, va_list *);
 int print_output(const char *format, va_list *);
-int check_for_format(*char, va_list *, int, int *count); /*tengo duda con los punteros here, as usual */
+int check_for_format(char, va_list *, int, int *count);
 void handle_errors(int);
 
 #endif /* HOLBERTON_H */
